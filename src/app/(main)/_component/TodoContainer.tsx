7e78e2932ee17a6a11e4c6e1f97427878c0cd3e0
vkak006 <vkak006@datasolution.kr>
2024-08-13 11:15:31 +0900

    Feat: TodoContainer Header 영역 정의

--- src/app/(main)/_component/TodoContainer.tsx
+++ src/app/(main)/_component/TodoContainer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import classNames from 'classnames/bind';
 
 import styles from './TodoContainer.module.css';
@@ -8,8 +9,13 @@ const cx = classNames.bind(styles);
 export default function TodoContainer() {
   return (
     <article className={cx('wrap')}>
-      <section className={cx('header')}>Header</section>
-      <section className={cx('contents')}>Contents</section>
+      <section className={cx('header')}>
+        <div className={cx('title')}>
+          <span>To-Do List</span>
+          <Image src="/images/icon.png" alt="note" width={32} height={32} />
+        </div>
+      </section>
+      <section className={cx('contents')}>Body</section>
     </article>
   );
 }
